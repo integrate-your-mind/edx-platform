a25ca73859f9041762b6cae304f75a0d15ed4558
Aarif <mraarif@outlook.com>
2020-04-29 18:51:58 +0500

    reverted back urllib change

--- lms/djangoapps/courseware/views/index.py
+++ lms/djangoapps/courseware/views/index.py
@@ -8,6 +8,7 @@ View for Courseware Index
 import logging
 
 import six
+import six.moves.urllib as urllib
 from django.conf import settings
 from django.contrib.auth.models import User
 from django.contrib.auth.views import redirect_to_login
